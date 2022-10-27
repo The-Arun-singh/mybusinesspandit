db4f7e13459f89e940b0810ad33104d9b5cfe93e
The-Arun-singh <arsinghbusiness@gmail.com>
2022-10-27 22:09:19 +0530

    mod-8.4.0(responsive)

--- js/index.js
+++ js/index.js
@@ -1,35 +1,14 @@
-let navList = document.querySelector('.list');
-let openBtn = document.getElementById("open_btn");
-let closeBtn = document.getElementById('close_btn');
+// let checkbox = document.querySelectorAll('nav> ul> li> input');
 
-function open() {
-    if (window.matchMedia('(max-width: 767px)').matches) {
-        for (let i=0; i< navList.childElementCount; i++) {
-            navList.children[i].style.display = 'block';
-        }
-        openBtn.style.display = 'none';
-        closeBtn.style.display = 'unset';
-    }
-}
-function close() {
-    if (window.matchMedia('(max-width: 767px)').matches) {
-        for (let i=0; i< navList.childElementCount; i++) {
-            navList.children[i].style.display = 'none';
-        }
-        openBtn.style.display = 'unset';
-        closeBtn.style.display = 'none';
-    }
-}
-// (function() {
-//     if (window.matchMedia('(min-width: 767px)').matches) {
-//         for (let i=0; i< navList.childElementCount; i++) {
-//             navList.children[i].style.display = 'block';
-//         }
-//         openBtn.style.display = 'none';
-//         closeBtn.style.display = 'none';
-//     } 
-// })();
+// function checked (e) {
+//     for (let i=0; i<checkbox.length; i++) {
+//         checkbox.checked = false;
+//         console.log(e.target.checked);
+//     }
 
+    
+// }
 
-openBtn.addEventListener("click", open);
-closeBtn.addEventListener('click', close);
+// document.querySelector('.list').addEventListener('click', checked)
+
+// console.log(checkbox);
