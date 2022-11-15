3b7a58b7e0e553178572debf6e4c749b4b8d3c6a
The-Arun-singh <arsinghbusiness@gmail.com>
2022-11-15 11:49:21 +0530

    mod-9.0.5

---
+++ js/fssai-reg.js
@@ -0,0 +1,23 @@
+// let prevBtn = document.querySelector('#prev-btn');
+// let nextBtn = document.querySelector('#next-btn');
+// let slides = document.querySelectorAll('.slide');
+// let slidesArr = Array.from(slides);
+// slides[0].setAttribute ('class','current');
+// let currSlide = document.querySelector('.current');
+
+// function next(slides,currSlide) {
+//     if (currSlide.className = 'current') {
+//         currSlide.nextElementSibling.setAttribute('class', 'current');
+//         currSlide.removeAttribute('class' , 'current');
+//     }
+// }
+
+// prevBtn.addEventListener('click', ()=>{
+//     // for (let i=0;i<n; i++) {
+//     //     slides[i].className= 'current';
+//     // }
+//     if (currSlide.className = 'current') {
+//         currSlide.nextElementSibling.setAttribute('class', 'current');
+//         currSlide.removeAttribute('class' , 'current');
+//     }
+// });
